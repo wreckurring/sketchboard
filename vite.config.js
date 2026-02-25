a5899ac90e303155ab3352cc508807821f80e64d
wreckurring <wreckurring@gmail.com>
2026-02-26 00:52:48 +0530

    update config

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,12 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  root: './', 
   server: {
     port: 5173
+  },
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true 
   }
 })
